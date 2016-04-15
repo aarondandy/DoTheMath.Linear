a20cf8ccdb74491bf1435b24ada0a9136f64d686
Aaron Dandy <aaron.dandy@live.com>
2016-04-14 23:59:31 -0700

    Setup main project

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#elif RELEASE
+[assembly: AssemblyConfiguration("Release")]
+#else
+[assembly: AssemblyConfiguration("Unknown")]
+#endif
+
+[assembly: AssemblyCompany("Aaron Dandy")]
+[assembly: AssemblyProduct("DoTheMath")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
