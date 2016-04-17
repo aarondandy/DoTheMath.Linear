bd240dd363dc3c6428071643665ca5a4f5f0b7f9
Aaron Dandy <aaron.dandy@live.com>
2016-04-16 22:56:31 -0700

    Better testing support for NCrunch and DNX

---
+++ src/DoTheMath.Linear/Vector3D.cs
@@ -0,0 +1,18 @@
+﻿namespace DoTheMath.Linear
+{
+    public struct Vector3D
+    {
+        public double X;
+
+        public double Y;
+
+        public double Z;
+
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
---
+++ test/DoTheMath.Linear.Tests/Vector3DTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace DoTheMath.Linear.Tests
+{
+    public class Vector3DTests
+    {
+        public class Constructors : Vector2DTests
+        {
+            [Fact]
+            public void default_constructor_sets_components_to_zero()
+            {
+                var v = new Vector3D();
+
+                Assert.Equal(v.X, 0.0d);
+                Assert.Equal(v.Y, 0.0d);
+                Assert.Equal(v.Z, 0.0d);
+            }
+
+            [Fact]
+            public void component_constructor_assigns_correct_fields()
+            {
+                var v = new Vector3D(1.0, -5.0, 6.0);
+
+                Assert.Equal(v.X, 1.0);
+                Assert.Equal(v.Y, -5.0);
+                Assert.Equal(v.Z, 6.0);
+            }
+        }
+    }
+}
